89704702920ac2ecc6f60d7e2121b43f7dcc17f5
meergit1 <meeralrafee57@gmail.com>
2024-09-15 23:00:38 -0400

    Add files via upload

---
+++ js01/project02/project01-02.js
@@ -0,0 +1,16 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Meer Alrafee
+    Date: 9/15/24  
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+var service1Name = "Basic"; service2Name="Express"; 
+var service3Name="Extreme"; service4Name = "Ultimate"; 
+
+var service1Speed = "50 Mbps"; service2Speed = "100 Mbps";
+var service3Speed = "500 Mbps"; service4Speed = "1 Gig";
