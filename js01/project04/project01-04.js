2d415b20de84e08685c041f5ae86017a88194ecc
meergit1 <meeralrafee57@gmail.com>
2024-09-28 21:58:55 -0400

    Add files via upload

---
+++ js01/project04/project01-04.js
@@ -0,0 +1,13 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Meer Alrafee
+    Date:   09/23/24
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
+var homeStreet= "1 Main St."; homeCity="Sicilia"; homeState="MA"; homeCode="02103"; workStreet = "15 Oak Ln."; workCity = "Central City"; workState = "MA"; workCode = "02104"
